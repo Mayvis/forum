36846fe29452b931a893dd5c0f74e6a143f03969
= <=>
2018-11-30 11:32:00 +0800

    First Class Search: Implementation

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index')->name('threads');
 Route::post('/threads', 'ThreadsController@store')->middleware('must-be-confirmed');
 Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/search', 'SearchController@show');
 Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 Route::patch('/threads/{channel}/{thread}', 'ThreadsController@update');
